ae823e43c7149c5455fd73dbb6156276874d8af6
designpigofficial <designpig@outlook.com>
2022-08-23 21:36:28 -0600

    build: FUCK HER

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,84 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 
 const Index = () => (
   <>
-    <Flex align={"center"} justify={"center"} w={"100%"} h={"100vh"}>
-      Hero
+    <Flex
+      align={"center"}
+      justify={"center"}
+      w={"100%"}
+      h={"100vh"}
+      flexDirection="column"
+      mt="200px"
+    >
+      <Heading pt="100px">
+        I recently decided to go in another direction... just one final word
+        her...
+      </Heading>
+      <Spacer />
+      <Text pb="200px" mt="100px">
+        So impressed with all you do
+        <br />
+        Tried so hard to be like you
+        <br />
+        Flew too high and burnt the wing
+        <br />
+        Lost my faith in everything
+        <br />
+        Lick around divine debris
+        <br />
+        Taste the wealth of hate in me
+        <br />
+        Shedding skin, succumb defeat
+        <br />
+        This machine is obsolete
+        <br />
+        Made the choice to go away
+        <br />
+        Drink the fountain of decay
+        <br />
+        Tear a hole exquisite red
+        <br />
+        Fuck the rest and stab it dead
+        <br />
+        Broken, bruised, forgotten sore
+        <br />
+        Too fucked up to care anymore
+        <br />
+        Poisoned to my rotten core
+        <br />
+        Too fucked up to care anymore
+        <br />
+        Broken, bruised, forgotten sore
+        <br />
+        Too fucked up to care anymore
+        <br />
+        Poisoned to my rotten core
+        <br />
+        Too fucked up to care anymore
+        <br />
+        In the back off the side far away
+        <br />
+        Is a place where I hide, where I stay
+        <br />
+        Tried to say, tried to ask I needed to
+        <br />
+        All alone by myself, where were you?
+        <br />
+        How could I ever think it's funny how?
+        <br />
+        Everything you swore it wouldn't change, is different now
+        <br />
+        Just like you would always say, "We'll make it through"
+        <em>Then my head fell apart and where were you?</em>
+        <br />
+        How could I ever think it's funny how? Everything you swore would never
+        change, is different now
+        <br />
+        <em>
+          Like you said, "You and me make it through" We didn't quite, it fell
+          apart, where the fuck were you?
+        </em>
+      </Text>
     </Flex>
   </>
 );
